e10173e7241a7ad90fe1c25850e27f509cac8988
Michael <mlandryviolin@gmail.com>
2014-10-13 00:54:41 -0400

    initial

---
+++ ANRLimited/src/landry/michael/ANR/model/Card.java
@@ -0,0 +1,81 @@
+package landry.michael.ANR.model;
+
+public class Card
+{
+	private String cardName;
+	private String faction;
+	private String type;
+	private int cost;
+	private int agenda;
+	private int pips;
+	private String effect;
+	private int idCorpCards;
+	public String getCardName()
+	{
+		return cardName;
+	}
+	public void setCardName(String cardName)
+	{
+		this.cardName = cardName;
+	}
+	public String getFaction()
+	{
+		return faction;
+	}
+	public void setFaction(String faction)
+	{
+		this.faction = faction;
+	}
+	public String getType()
+	{
+		return type;
+	}
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	public int getCost()
+	{
+		return cost;
+	}
+	public void setCost(int cost)
+	{
+		this.cost = cost;
+	}
+	public int getAgenda()
+	{
+		return agenda;
+	}
+	public void setAgenda(int agenda)
+	{
+		this.agenda = agenda;
+	}
+	public int getPips()
+	{
+		return pips;
+	}
+	public void setPips(int pips)
+	{
+		this.pips = pips;
+	}
+	public String getEffect()
+	{
+		return effect;
+	}
+	public void setEffect(String effect)
+	{
+		this.effect = effect;
+	}
+	public int getIdCorpCards()
+	{
+		return idCorpCards;
+	}
+	public void setIdCorpCards(int idCorpCards)
+	{
+		this.idCorpCards = idCorpCards;
+	}
+	
+	
+	
+
+}
---
+++ ANRLimited/src/landry/michael/ANR/model/CardBatch.java
@@ -0,0 +1,28 @@
+package landry.michael.ANR.model;
+
+import java.util.ArrayList;
+
+public class CardBatch
+{
+	private ArrayList<Card> cards;
+	private String header;
+	
+	public ArrayList<Card> getCards()
+	{
+		return cards;
+	}
+	public void setCards(ArrayList<Card> cards)
+	{
+		this.cards = cards;
+	}
+	public String getHeader()
+	{
+		return header;
+	}
+	public void setHeader(String header)
+	{
+		this.header = header;
+	}
+	
+
+}
